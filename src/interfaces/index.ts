0ed9c62b8b8be5883017722ad246d077e4bde35f
Tim <timothy.j.holm@gmail.com>
2020-11-03 10:38:08 +1100

    feat: WIP idiomatic nitric SDK for NodeJS.

---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+import { EventingClient } from './proto/eventing_grpc_pb'
+
+export * from './proto/eventing_grpc_pb';
+export * from './proto/eventing_pb';
+export * from './proto/storage_grpc_pb';
+export * from './proto/storage_pb';
+export * as grpc from '@grpc/grpc-js';
