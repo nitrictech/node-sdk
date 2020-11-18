5e6cd96431f22bc785bd4d61a4540c50a8f673e7
Tim <timothy.j.holm@gmail.com>
2020-11-18 12:51:54 +1100

    feat: WIP DocumentsClient implementation.

--- src/v1.ts
+++ src/v1.ts
@@ -2,11 +2,95 @@ import process from 'process';
 
 import { v1 } from './interfaces';
 import * as grpc from '@grpc/grpc-js';
+import { CreateDocumentRequest, DeleteDocumentRequest, GetDocumentRequest, UpdateDocumentRequest } from './interfaces/v1';
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
 
 const AMBASSADOR_ADDRESS = process.env.AMBASSADOR_ADDRESS || '127.0.0.1';
 const AMBASSADOR_PORT = process.env.AMBASSADOR_PORT || '50051';
 
 const AMBASSADOR_BIND = `${AMBASSADOR_ADDRESS}:${AMBASSADOR_PORT}`;
+
+export class DocumentsClient<T extends {[key:string]: any}> {
+  private grpcClient: v1.DocumentsClient;
+  private collection: string;
+
+  constructor(collection: string) {
+    this.collection = collection;
+    this.grpcClient = new v1.DocumentsClient(
+      AMBASSADOR_BIND, 
+      grpc.ChannelCredentials.createInsecure()
+    );
+  }
+
+  async createDocument(key: string, document: T): Promise<Empty> {
+    const request = new CreateDocumentRequest();
+    request.setCollection(this.collection);
+    request.setKey(key);
+    request.setDocument(Struct.fromJavaScript(document));
+
+    return new Promise<Empty>((resolve, reject) => {
+      this.grpcClient.createDocument(request, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+
+  async getDocument(key: string): Promise<T> {
+    const request = new GetDocumentRequest();
+    request.setCollection(this.collection);
+    request.setKey(key);
+
+    return new Promise<T>((resolve, reject) => {
+      this.grpcClient.getDocument(request, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          const document = response.getDocument();
+
+          resolve(document.toJavaScript() as T);
+        }
+      });
+    });
+  }
+
+  async updateDocument(key: string, document: T): Promise<Empty> {
+    const request = new UpdateDocumentRequest();
+    request.setCollection(this.collection);
+    request.setKey(key);
+    request.setDocument(Struct.fromJavaScript(document));
+
+    return new Promise<Empty>((resolve, reject) => {
+      this.grpcClient.updateDocument(request, (error, response) => {
+        if (error) {
+          reject(error);
+        }
+
+        resolve(response);
+      })
+    });
+  }
+
+  async deleteDocument(key: string): Promise<Empty> {
+    const request = new DeleteDocumentRequest();
+    request.setCollection(this.collection);
+    request.setKey(key);
+
+    return new Promise<Empty>((resolve, reject) => {
+      this.grpcClient.deleteDocument(request, (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      })
+    });
+  }
+}
 
 /**
  * 
@@ -33,9 +117,9 @@ export class EventingClient {
     });
   }
 
-  async publish(request: v1.PublishRequest): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.grpcClient.publish(request, (error, response: void) => {
+  async publish(request: v1.PublishRequest): Promise<Empty> {
+    return new Promise<Empty>((resolve, reject) => {
+      this.grpcClient.publish(request, (error, response) => {
         if (error) {
           reject(error); 
         } else {
