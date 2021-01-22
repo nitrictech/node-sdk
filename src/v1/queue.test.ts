0184dd3b2f1d1f096d7b939f893c85c7cf14f2e4
Tim <timothy.j.holm@gmail.com>
2021-01-22 13:45:20 +1100

    chore: queue client test coverage at 100%.

--- src/v1/queue.test.ts
+++ src/v1/queue.test.ts
@@ -1,7 +1,8 @@
 import { QueueClient } from "./queue";
 
 import { queue } from "../interfaces/v1";
-import { PushResponse } from "../interfaces/v1/queue";
+import { FailedMessage, NitricEvent, PushResponse } from "../interfaces/v1/queue";
+import { Struct } from "google-protobuf/google/protobuf/struct_pb";
 const { QueueClient: GrpcQueueClient } = queue;
 
 describe("Queue Client Tests", () => {
@@ -61,6 +62,53 @@ describe("Queue Client Tests", () => {
         }
       }])).resolves.toEqual([]);
     });
+  })
 
+  describe("Given nitric.v1.queue.Push partially succeeds", () => {
+    const mockEvents = [{
+      requestId: "test",
+      payloadType: "Test Payload",
+      payload: {
+        test: "test"
+      }
+    }]
+
+    beforeAll(() => {
+      jest.spyOn(GrpcQueueClient.prototype, "push").mockImplementation((request, callback: any) => {
+        const mockResponse = new PushResponse()
+        mockResponse.setFailedmessagesList(mockEvents.map(e => {
+          const msg = new FailedMessage()
+          const evt = new NitricEvent()
+          evt.setRequestid(e.requestId);
+          evt.setPayloadtype(e.payloadType);
+          evt.setPayload(Struct.fromJavaScript(e.payload))
+          msg.setEvent(evt);
+          msg.setMessage("Failed to Push event");
+
+          return msg;
+        }))
+        callback(null, mockResponse);
+
+        return null as any;
+      });
+    });
+
+    afterAll(() => {
+      jest.resetAllMocks();
+    });
+
+    test("Then EventingClient.publish should resolve with no failed messages", () => {
+      const client = new QueueClient();
+      expect(client.push("test", [{
+        requestId: "test",
+        payloadType: "Test Payload",
+        payload: {
+          test: "test"
+        }
+      }])).resolves.toEqual([{
+        event: mockEvents[0],
+        message: "Failed to Push event",
+      }]);
+    });
   })
 });
