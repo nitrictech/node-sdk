8bfaa5fc6e09aea32844fe3197bbf94ef35d9960
davemooreuws <davemooreuws@gmail.com>
2021-03-24 15:26:11 +1100

    chore: add paths to tsconfig to cleanup code

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	// Ignore the auto generated code for code coverage
-	coveragePathIgnorePatterns: ["/interfaces/"]
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // Ignore the auto generated code for code coverage
+  coveragePathIgnorePatterns: ["/interfaces/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
