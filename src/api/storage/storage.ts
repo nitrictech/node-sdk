befb4debdea17cee962a4d11af4fa36bf6a48473
Tim Holm <tim.holm@nitric.io>
2021-04-14 12:22:37 +1000

    docs: Add typedoc comments to the StorageClient.

--- src/api/storage/storage.ts
+++ src/api/storage/storage.ts
@@ -3,7 +3,7 @@ import { storage } from "../../interfaces";
 import * as grpc from "@grpc/grpc-js";
 
 /**
- *
+ * Nitric queue client, facilitates writing and reading from blob storate (buckets).
  */
 export class StorageClient {
   private grpcClient: storage.StorageClient;
@@ -15,6 +15,21 @@ export class StorageClient {
     );
   }
 
+  /**
+   * Write a an array of bytes to a bucket
+   * @param bucket The bucket to write to
+   * @param key The key/path of the item to write
+   * @param body The contents to write
+   * 
+   * Example:
+   * ```typescript
+   * import { StorageClient } from "@nitric/sdk";
+   * 
+   * const client = new StorageClient();
+   * 
+   * await client.write("my-bucket", "my-item", Buffer.from("My Test File..."));
+   * ```
+   */
   async write(bucket: string, key: string, body: Uint8Array): Promise<boolean> {
     const request = new storage.StorageWriteRequest();
     request.setBucketname(bucket);
@@ -32,6 +47,20 @@ export class StorageClient {
     });
   }
 
+  /**
+   * Read an array of bytes from a bucket
+   * @param bucket The bucket to read from
+   * @param key The key of the blob item to read
+   * 
+   * Example:
+   * ```typescript
+   * import { StorageClient } from "@nitric/sdk";
+   * 
+   * const client = new StorageClient();
+   * 
+   * const bytes = await client.read("my-bucket", "my-item");
+   * ```
+   */
   async read(bucket: string, key: string): Promise<Uint8Array> {
     const request = new storage.StorageReadRequest();
     request.setBucketname(bucket);
