836c389778d684ee3a5ce328917aeb0f6b8e4eb6
Jye Cusch <jye.cusch@nitric.io>
2022-01-04 12:37:48 +1100

    feat: add collection registration

--- src/resources/collection.ts
+++ src/resources/collection.ts
@@ -1,25 +1,63 @@
+import { Resource, ResourceDeclareRequest, ResourceDeclareResponse, ResourceType } from "@nitric/api/proto/resource/v1/resource_pb";
+import resourceClient from './client';
 
-class CollectionRef {
+class CollectionRef {}
 
-}
+type CollectionPermission = 'reading' | 'writing' | 'deleting';
 
-type CollectionPermission = "reading" | "writing" | "deleting";
+const everything: CollectionPermission[] = ['reading', 'writing', 'deleting'];
 
-const everything: CollectionPermission[] = ["reading", "writing", "deleting"];
-
+/**
+ * A document collection resources, such as a collection/table in a document database.
+ */
 class CollectionResource {
-	private readonly name: string;
+  private readonly name: string;
 
-	constructor(name: string) {
-		this.name = name;
-	}
+  constructor(name: string) {
+    this.name = name;
+  }
 
-	public for(perm: CollectionPermission[] | CollectionPermission, ...perms: CollectionPermission[]): CollectionRef {
-		// TODO: call server to request permissions.
-		return new CollectionRef();
-	}
+  private async register(): Promise<void> {
+    const req = new ResourceDeclareRequest();
+    const resource = new Resource();
+    resource.setName(this.name);
+    resource.setType(ResourceType.COLLECTION);
+    req.setResource(resource);
+
+    return new Promise<void>((resolve, reject) => {
+      resourceClient.declare(
+        req,
+        (error, response: ResourceDeclareResponse) => {
+          if (error) {
+            // TODO: remove this ignore when not using link
+            // @ts-ignore
+            reject(fromGrpcError(error));
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
+
+  public for(
+    perm: CollectionPermission[] | CollectionPermission,
+    ...perms: CollectionPermission[]
+  ): CollectionRef {
+    // TODO: call server to request permissions.
+    return new CollectionRef();
+  }
 }
+
+// This singleton helps avoid duplicate references to collection('name')
+// will return the same collection resource
+const collections: Record<string, CollectionResource> = {};
 
 export const collection = (name: string): CollectionResource => {
-	return new CollectionResource(name);
-}
+  if (!collections[name]) {
+    collections[name] = new CollectionResource(name);
+    collections[name]['register']();
+  }
+
+  return collections[name];
+};
