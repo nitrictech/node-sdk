a2efe19b36471189a4e14c361082becf8f3819a1
Jye Cusch <jye.cusch@nitric.io>
2022-01-04 12:36:27 +1100

    feat: add queue registration

--- src/resources/queue.ts
+++ src/resources/queue.ts
@@ -1,4 +1,6 @@
+import { Resource, ResourceDeclareRequest, ResourceDeclareResponse, ResourceType } from "@nitric/api/proto/resource/v1/resource_pb";
 import { Task } from "src/types";
+import resourceClient from './client';
 import { queues, Queue } from "../api/"; 
 
 type QueuePermission = "sending" | "receiving";
@@ -14,6 +16,29 @@ class QueueResource {
 		this.name = name;
 	}
 
+	private async register(): Promise<void> {
+    const req = new ResourceDeclareRequest();
+    const resource = new Resource();
+    resource.setName(this.name);
+    resource.setType(ResourceType.QUEUE);
+    req.setResource(resource);
+
+    return new Promise<void>((resolve, reject) => {
+      resourceClient.declare(
+        req,
+        (error, response: ResourceDeclareResponse) => {
+          if (error) {
+            // TODO: remove this ignore when not using link
+            // @ts-ignore
+            reject(fromGrpcError(error));
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
+
 	public for(perm: QueuePermission[] | QueuePermission, ...perms: QueuePermission[]): Queue {
 		// TODO: call server to request permissions.
 		return queues().queue(this.name);
@@ -26,6 +51,7 @@ const queuesCache: Record<string, QueueResource> = {};
 export const queue = (name: string): QueueResource => {
 	if(!queuesCache[name]) {
 		queuesCache[name] = new QueueResource(name);
+		queuesCache[name]['register']();
 	}
 
 	return queuesCache[name];
