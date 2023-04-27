37d482ec45d6df4951164bcacfd927d81bd48ad0
Ryan Cartwright <fryzee.cartwright@gmail.com>
2023-04-28 09:28:01 +1000

    feat: remove logging

--- src/faas/v0/context.ts
+++ src/faas/v0/context.ts
@@ -465,9 +465,6 @@ export class BucketNotificationContext extends TriggerContext<
 
     const attributes = objectFromMap(notification.getAttributesMap());
 
-    console.log(`Raw Attributes: ${notification.getAttributesMap()}`);
-    console.log(`Attributes: ${attributes}`);
-
     ctx.request = new BucketNotificationRequest(
       trigger.getData_asU8(),
       getTraceContext(trigger.getTraceContext()),
