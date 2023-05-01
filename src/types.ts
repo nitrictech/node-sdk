ae7c395ff2682bb7708c1390a8ad68e12a1849b7
Tim Holm <tim.holm@nitric.io>
2023-05-02 09:47:38 +1000

    cleanup commented code.

--- src/types.ts
+++ src/types.ts
@@ -11,27 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// export interface NitricEvent<
-//   T extends Record<string, any> = Record<string, any>
-// > {
-//   /**
-//    * Uniquely identifies the event.
-//    *
-//    * Within your app you must ensure the ID is unique.
-//    * Subscribers can assume events with the same ID are duplicates and avoid reprocessing them
-//    */
-//   id?: string;
-//   /**
-//    * An optional description of the event type.
-//    *
-//    * Can be useful for de-serialization, routing or observability. The format of this value is determined by the producer.
-//    */
-//   payloadType?: string;
-//   /**
-//    * The event's payload data, with details of the event.
-//    */
-//   payload: T;
-// }
 
 export class NitricEvent<T extends Record<string, any> = Record<string, any>> {
   public readonly payload: T;
@@ -64,31 +43,6 @@ export class NitricTask<T extends Record<string, any> = Record<string, any>> {
     this.payloadType = payloadType;
   }
 }
-
-// export interface Task<T extends Record<string, any> = Record<string, any>> {
-//   /**
-//    * Uniquely identifies the task.
-//    *
-//    * Within your app you must ensure the ID is unique.
-//    */
-//   id?: string;
-//   /**
-//    * The ID for the current lease of this task.
-//    *
-//    * A task may be leased multiple times, resulting in new lease IDs.
-//    */
-//   leaseId?: string;
-//   /**
-//    * An optional description of the task type.
-//    *
-//    * Can be useful for de-serialization, routing or observability. The format of this value is determined by the producer.
-//    */
-//   payloadType?: string;
-//   /**
-//    * The task's payload data, with details of the task or work to be done.
-//    */
-//   payload?: Record<string, any>;
-// }
 
 export type WhereQueryOperator =
   | '<'
