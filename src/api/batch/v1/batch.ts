ffed2a788663c08648383362d8af8384d8a5f9a8
David Moore <davemooreuws@gmail.com>
2024-09-05 10:14:52 +1000

    add missing license headers

--- src/api/batch/v1/batch.ts
+++ src/api/batch/v1/batch.ts
@@ -1,3 +1,16 @@
+// Copyright 2021, Nitric Technologies Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import { SERVICE_BIND } from '@nitric/sdk/constants';
 import { BatchClient } from '@nitric/sdk/gen/nitric/proto/batch/v1/batch_grpc_pb';
 import * as grpc from '@grpc/grpc-js';
